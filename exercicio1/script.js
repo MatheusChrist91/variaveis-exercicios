ce6b0ab056ff74bc4266426f2990007cd909ebf3
MatheusChrist91 <123036490+MatheusChrist91@users.noreply.github.com>
2023-01-31 23:32:56 -0300

    Exercicios feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,37 @@
+// Exercícios Variáveis Parte 1
+
+/* Variáveis
+let nome = prompt('Qual é o seu nome?')
+let idade = Number(prompt('Qual é a sua idade?'))
+
+console.log(typeof(nome), typeof idade)
+
+
+resposta d: para que o valor da variável nao fique indefinido
+
+
+console.log ('resposta f: A variável número veio como tipo: String (foi corrigido)')
+
+console.log ('Olá', nome, 'você tem', idade,'anos') 
+*/
+
+// Exercícios Variáveis Parte 2
+
+// Variáveis 
+
+let filme = 'você gosta de filmes?'
+let series = 'você gosta de seriados?'
+let musica = 'voce gosta de música?'
+
+
+let filmer = prompt(filme)
+let seriesr = prompt(series)
+let musicar = prompt(musica)
+
+console.log(filmer, seriesr, musicar)
+
+console.log(
+${filme} - ${filmer}
+${series} - ${seriesr}
+${musica} - ${musicar}
+)
